8faf4556faba499a850c81ce2ceaec6a2cc90a0d
Antonio Hernandez Souza <ahernandezsouza@gmail.com>
2016-08-12 16:34:41 -0500

    Enemies show (but don't move

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(dt, x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.dt = dt;
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,13 +26,51 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(dt) {
+    var obj = Object.create(Enemy.prototype);
+    obj.dt = dt;
+    this.sprite = 'images/char-boy.png';
+    return obj;
+}
 
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var tile = {"width" : 101,
+            "height" : 62,
+            "vpadding" : 10,
+            "y": function(y){
+                var tiley = tile.height + y * (tile.height + tile.vpadding * 2);
+                return tiley;
+            },
+            "x": function(x){
+                var tilex = x * (tile.width);
+                return tilex;
+            }
+        };
 
+var enemy1 = new Enemy(1, tile.x(0), tile.y(0));
+var enemy2 = new Enemy(1, tile.x(1), tile.y(1));
+var enemy3 = new Enemy(1, tile.x(2), tile.y(2));
+
+var allEnemies = [
+    enemy1,
+    enemy2,
+    enemy3
+];
+
+var player = Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
